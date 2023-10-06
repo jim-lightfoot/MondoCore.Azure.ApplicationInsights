097c374deea48e6e1d2b452b693c73407fc6b02d
Jim Lightfoot <jim.lightfoot@outlook.com>
2023-10-06 15:32:01 -0700

    Code moved from MondoCore.Azure. Removed MondoCore.Common dependencies. Updated libraries

---
+++ MondoCore.Azure.ApplicationInsights/ISupportPropertiesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.ApplicationInsights.DataContracts;
+
+using MondoCore.Log;
+
+namespace MondoCore.Azure.ApplicationInsights
+{
+    internal static class ISupportPropertiesExtensions
+    {
+        internal static void MergeProperties(this ISupportProperties aiTelemetry, Telemetry telemetry, bool childrenAsJson)
+        {
+            var props = telemetry.Properties?.ToStringDictionary(childrenAsJson);
+
+            if(props == null || props.Count == 0)
+                return;
+
+            aiTelemetry.Properties.Merge(props);
+        }
+    }
+}
---
+++ test/MondoCore.ApplicationInsights.FunctionalTests/ApplicationInsights.cs
@@ -0,0 +1,136 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AppInsights = MondoCore.Azure.ApplicationInsights.ApplicationInsights;
+using MondoCore.Log;
+using Newtonsoft.Json;
+
+namespace MondoCore.ApplicationInsights.FunctionalTests
+{
+    [TestClass]
+    [TestCategory("Functional Tests")]
+    public class ApplicationInsightsTests
+    {
+        private string _correlationId = Guid.NewGuid().ToString().ToLower();
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteException()
+        {
+            var log = CreateAppInsights();
+             
+            await log.WriteError(new System.Exception("Test Exception"), properties: new { Make = "Chevy", Model = "Camaro", Year = 1969 });
+        }
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteEvent()
+        {
+            var log = CreateAppInsights();
+             
+            await log.WriteEvent("Test Event", properties: new { Make = "Chevy", Model = "Camaro", Year = 1969 });
+        }
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteError()
+        {
+            using(var log = SetupRequest("WriteError"))
+            { 
+                log.SetProperty("Model", "Corvette");
+
+                await log.WriteError(new Exception("Bob's hair is on fire"), properties: new {Make = "Chevy", Engine = new { Cylinders = 8, Displacement = 350, Piston = new { RodMaterial = "Chrome Moly", Material = "Stainless Steel", Diameter = 9200 } } } );
+                await log.WriteError(new Exception("Fred's hair is on fire"), properties: new {Make = "Chevy" } );
+                await log.WriteError(new Exception("Wilma's hair is on fire"), properties: new {Make = "Chevy" } );
+            }
+        }
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteError_dotted()
+        {
+            using(var log = SetupRequest("WriteError", false))
+            { 
+                log.SetProperty("Model", "Corvette");
+
+                await log.WriteError(new Exception("Bob's hair is on fire"), properties: new {Make = "Chevy", Engine = new { Cylinders = 8, Displacement = 350, Piston = new { RodMaterial = "Chrome Moly", Material = "Stainless Steel", Diameter = 9200 } } } );
+                await log.WriteError(new Exception("Fred's hair is on fire"), properties: new {Make = "Chevy" } );
+                await log.WriteError(new Exception("Wilma's hair is on fire"), properties: new {Make = "Chevy" } );
+            }
+        }
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteError2()
+        {
+            using(var log = SetupRequest("WriteError2"))
+            { 
+                log.SetProperty("Make", "Chevy");
+                log.SetProperty("Model", "Corvette");
+
+                await log.WriteEvent("John's feet are wet");
+                await log.WriteError(new Exception("George's hair is on fire"));
+                await log.WriteError(new Exception("Franks's hair is on fire"));
+                await log.WriteError(new Exception("Naomi's hair is on fire"), Telemetry.LogSeverity.Critical);
+            }
+        }
+
+        [TestMethod]
+        public async Task ApplicationInsights_WriteError3()
+        {
+            using(var log = SetupRequest("WriteError3"))
+            { 
+                 await log.WriteEvent("John's hair is on fire", properties: new {Make = "Chevy", Model = "Corvette" } );
+                 await log.WriteError(new Exception("Linda's hair is on fire"), properties: new {Make = "Chevy", Model = "Corvette" } );
+            }
+        }
+
+        #region Private
+
+        private IRequestLog SetupRequest(string operationName, bool childrenAsJson = true)
+        {
+            var baseLog = new MondoCore.Log.Log();
+
+            baseLog.Register(CreateAppInsights(childrenAsJson));
+
+            return new RequestLog(baseLog, operationName, _correlationId);
+        }
+
+
+        private AppInsights CreateAppInsights(bool childrenAsJson = true)
+        { 
+            var config = TestConfiguration.Load();
+            var tConfig = new TelemetryConfiguration { ConnectionString = config.InstrumentationKey };
+
+            return new AppInsights(tConfig, childrenAsJson);
+        }
+
+        #endregion
+
+    }
+
+    internal static class TestConfiguration
+    {
+        public static Configuration Load()
+        { 
+            var path = Assembly.GetCallingAssembly().Location;
+                path = path[..path.IndexOf("\\bin")];
+            var json = File.ReadAllText(Path.Combine(path, "localhost.json"));
+
+            return JsonConvert.DeserializeObject<Configuration>(json);
+        }
+    }
+
+    internal class Configuration
+    {
+        public string InstrumentationKey        { get; set; }
+        public string ConnectionString          { get; set; }
+        public string DataLakeConnectionString          { get; set; }
+        public string ConfigConnectionString    { get; set; }
+
+        public string KeyVaultUri               { get; set; }
+        public string KeyVaultTenantId          { get; set; }
+        public string KeyVaultClientId          { get; set; }
+        public string KeyVaultClientSecret      { get; set; }     
+    }    
+}
